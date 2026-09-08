4c0fb16404e4332c472bbab9a90921a99c46c1a8
John Francis Torres <johnfrancistorres12@gmail.com>
2026-09-08 21:36:56 +0800

    Route home page to login

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,7 +44,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-$router->get('/', 'Welcome::index');
+$router->get('/', 'AuthController::login');
 $router->get('/student', 'StudentController::index');
 $router->get('/student/profile', 'StudentController::profile')->middleware('student');
 $router->get('/users', 'UsersController::index');
