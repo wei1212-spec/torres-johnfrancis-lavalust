b3bb343c9aaa24771a68b6407e267cd57418a823
John Francis Torres <johnfrancistorres12@gmail.com>
2026-09-09 14:25:53 +0800

    push

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -7,7 +7,7 @@ class StudentController extends Controller
     private $student = [
         'student_id'  => 'MCC2024-00104',
         'name'        => 'Apostol, rhemz jhon Cyrus C.',
-        'course'      => 'BSIT',
+        'course'      => 'BSIT it',
         'year'        => '3rd Year',
         'section'     => '3-F3',
         'email'       => 'rhemzjhoncyruscapostol@gmail.com',
