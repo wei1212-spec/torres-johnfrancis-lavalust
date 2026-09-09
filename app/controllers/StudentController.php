9836ab0cbb68fe78b100c1711facf1d0bd4ecc4d
torres <johnfrancistorres12@gmail.com>
2026-09-09 16:42:40 +0800

    Update student controller

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -6,7 +6,7 @@ class StudentController extends Controller
 {
     
     private $student = [
-        'student_id'  => 'MCC2024-00104',
+        'student_id'  => 'MCC2024-00104', 
         'name'        => ', rhemz jhon Cyrus C.',
         'course'      => 'BSIT',
         'year'        => '3rd Year',
